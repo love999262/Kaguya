d2ea2c979f1d5263045833518f1349e8c3438cb3
Sun Yuguang <sun87959012@hotmail.com>
2017-03-06 01:26:34 +0800

    add ripples

--- src/js/Kaguya.js
+++ src/js/Kaguya.js
@@ -1,11 +1,22 @@
-require('./../css/kaguya.less');
-window.$ = require('./jquery-2.1.4.min.js');
-var snowfall = require('./snowfall.jquery.js');
+// require('./../css/kaguya.less');
+// window.$ = require('./jquery-2.1.4.min.js');
+// var snowfall = require('./snowfall.jquery.js');
+// var ripples = require('./jquery.ripples.js');
 var Kaguya = {
     init: function(image) {
+        this.setStyle();
         this.setBackground('body', './images/' + image + '');
-        this.setSnow('body', './images/snow.png');
-        // this.setSnow(ele);
+        // this.setSnow('body', './images/snow.png');
+        this.setRipples('body');
+        // this.setVideo();
+        this.setSnow('body');
+        this.autoDrop();
+    },
+    setStyle: function() {
+        $('*').css({
+            'margin': 0,
+            'padding': 0
+        })
     },
     setBackground: function(ele, url) {
         $(ele).css({
@@ -24,24 +35,49 @@ var Kaguya = {
     setSnow: function(ele, url) {
         if(arguments.length === 1) {
             $(ele).snowfall({
-                minSize: 2,
-                maxSize: 8,
+                minSize: 4,
+                maxSize: 12,
                 round: true,
-                // minSpeed: 5,
-                // maxSpeed: 10,
-                shadow: true
+                minSpeed: 6,
+                maxSpeed: 8,
+                shadow: true,
+                round: true
             });
         } else if(arguments.length === 2) {
             $(ele).snowfall({
                 image: url,
-                minSize: 8,
-                maxSize: 24,
-                // minSpeed: 5,
-                // maxSpeed: 10,
+                minSize: 4,
+                maxSize: 12,
+                round: true,
+                minSpeed: 6,
+                maxSpeed: 8,
+                // shadow: true,
                 round: true
             });
         }
-
+    },
+    setRipples: function(ele) {
+        $(ele).ripples('play');
+        $(ele).ripples({
+            resolution: 512,
+            dropRadius: 20,
+            perturbance: 0.04,
+        });        
+    },
+    autoDrop: function() {
+        setInterval(function() {
+            var $el = $('body');
+            var x = Math.random() * $el.outerWidth();
+            var y = Math.random() * $el.outerHeight();
+            var dropRadius = 20;
+            var strength = 0.04 + Math.random() * 0.04;
+            $el.ripples('drop', x, y, dropRadius, strength);
+        }, (1000 / 60) * 40);
+    },
+    setVideo: function() {
+        var _audio = '<audio src="../video/kaguya.mp3" autoplay></audio>'
+        $('body').append(_audio);
     }
+
 };
 Kaguya.init('kaguya.jpg');
